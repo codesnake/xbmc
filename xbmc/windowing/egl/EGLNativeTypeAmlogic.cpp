ada68794e9b8e574b648d165d1e5c0ade293de5a
Alex Deryskyba <alex@codesnake.com>
2014-07-23 22:23:34 +0300

    Add support for AV (CVBS) video output on Amlogic
    Conflicts:
    
    	xbmc/windowing/egl/EGLNativeTypeAmlogic.cpp

--- xbmc/windowing/egl/EGLNativeTypeAmlogic.cpp
+++ xbmc/windowing/egl/EGLNativeTypeAmlogic.cpp
@@ -160,6 +160,10 @@ bool CEGLNativeTypeAmlogic::SetNativeResolution(const RESOLUTION_INFO &res)
           else
             SetDisplayResolution("1080p");
           break;
+        case 720:
+          if (!IsHdmiConnected())
+            SetDisplayResolution("480cvbs");
+          break;
       }
       break;
     case 50:
@@ -174,6 +178,10 @@ bool CEGLNativeTypeAmlogic::SetNativeResolution(const RESOLUTION_INFO &res)
             SetDisplayResolution("1080i50hz");
           else
             SetDisplayResolution("1080p50hz");
+          break;
+        case 720:
+          if (!IsHdmiConnected())
+            SetDisplayResolution("576cvbs");
           break;
       }
       break;
@@ -190,9 +198,18 @@ bool CEGLNativeTypeAmlogic::SetNativeResolution(const RESOLUTION_INFO &res)
 
 bool CEGLNativeTypeAmlogic::ProbeResolutions(std::vector<RESOLUTION_INFO> &resolutions)
 {
-  std::string valstr;
-  SysfsUtils::GetString("/sys/class/amhdmitx/amhdmitx0/disp_cap", valstr);
-  std::vector<std::string> probe_str = StringUtils::Split(valstr, "\n");
+  std::vector<std::string> probe_str;
+  if (IsHdmiConnected())
+  {
+    std::string valstr;
+    SysfsUtils::GetString("/sys/class/amhdmitx/amhdmitx0/disp_cap", valstr);
+    probe_str = StringUtils::Split(valstr, "\n");
+  }
+  else
+  {
+    probe_str.push_back("480cvbs");
+    probe_str.push_back("576cvbs");
+  }
 
   resolutions.clear();
   RESOLUTION_INFO res;
@@ -210,8 +227,11 @@ bool CEGLNativeTypeAmlogic::GetPreferredResolution(RESOLUTION_INFO *res) const
   // check display/mode, it gets defaulted at boot
   if (!GetNativeResolution(res))
   {
-    // punt to 720p if we get nothing
-    aml_mode_to_resolution("720p", res);
+    // punt to 720p or 576cvbs if we get nothing
+    if (IsHdmiConnected())
+      aml_mode_to_resolution("720p", res);
+    else
+      aml_mode_to_resolution("576cvbs", res);
   }
 
   return true;
@@ -342,4 +362,11 @@ void CEGLNativeTypeAmlogic::SetFramebufferResolution(int width, int height) cons
     }
     close(fd0);
   }
+}
+
+bool CEGLNativeTypeAmlogic::IsHdmiConnected() const
+{
+  char hpd_state[2] = {0};
+  aml_get_sysfs_str("/sys/class/amhdmitx/amhdmitx0/hpd_state", hpd_state, 2);
+  return hpd_state[0] == '1';
 }
